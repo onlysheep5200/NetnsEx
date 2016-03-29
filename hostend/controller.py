ae02c7e7b68f4008a9bec769792ea91885ffa0e2
hyd <342159837@qq.com>
2016-03-29 17:09:25 +0800

    添加测试

---
+++ hostend/controller.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import requests
+import json
+class Controller(object):
+
+    reportUrl = None
+    requestUrl = None
+
+    def report(self,event):
+        #requests.post(self.reportUrl,event)
+        pass
+
+    def request(self,resources):
+        #requests.get(requests,data=dict(resources=resources))
+        pass
+
+
+class Events(object):
+
+    @classmethod
+    def container_created_event(cls,container,netns=None):
+        return {
+            'type' : 'container_created',
+            'container' : json.dumps(container),
+            'is_netns_created' : netns == None,
+            'netns' : json.dumps(netns)
+        }
+
+
+
+
---
+++ test/test_proxy.py
@@ -0,0 +1,5 @@
+from hostend.proxy import *
+from lib.utils import Host
+if __name__ == '__main__' :
+    proxy = DockerProxy(Client('unix://var/run/docker.sock'),Host.currentHost('1212',switchInterface='ovsbr1'))
+    proxy.create_container('10.232.0.3',None,image='ubuntu',command='/bin/sh',stdin_open=True,tty=True,detach=True)
